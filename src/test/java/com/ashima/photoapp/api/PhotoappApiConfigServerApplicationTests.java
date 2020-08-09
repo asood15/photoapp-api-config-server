858948c6d0d5cafcb2a9340e5e5dab7fd9842d9d
Ashima Sood <ashima.sood15@gmail.com>
2020-08-09 12:34:16 +0200

    Spring Cloud Config server
    Added a spring cloud config server which publishes any changes in the native or git applicaion file to all running microservices. RabbitMQ is used as a messaging broker

---
+++ src/test/java/com/ashima/photoapp/api/PhotoappApiConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ashima.photoapp.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoappApiConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
